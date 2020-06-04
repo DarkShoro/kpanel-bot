e2698a0d9e2e43331a2a7d7ba2bcca5be218bf1a
FireWolf <62524520+FireWolf-kPanel@users.noreply.github.com>
2020-06-04 15:03:39 +0200

    Add files via upload

---
+++ Events/message.js
@@ -0,0 +1,19 @@
+const prefix = "k!";
+
+module.exports = async(client, message) => {
+
+    if(message.author.bot) return;
+    if(message.channel.type === "dm") return;
+
+    if(!message.content.startsWith(prefix)) return;
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const command = args.shift();
+
+    const cmd = client.commands.get(command);
+
+    if(!cmd) return;
+    
+    message.delete().catch();
+    cmd.run(client, message, args);
+};
